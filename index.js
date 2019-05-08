040f187f026aec7bb5bea70ec188a21d81305c7d
xingbaofeng <xingbaofeng@xdf.cn>
2019-05-08 09:47:55 +0800

    init

--- index.js
+++ index.js
@@ -3,4 +3,3 @@
 <p class="spell_end bg_gray" style="display: none">直播已结束</p>
 <p class="spell_seeback" style="display: none;background: #0486FE;color: #fff;flex: 1 100%;">查看回放</p>
 <p class="spell_already_buy study_show" style="display: none">立即报名</p>
-<p class="spell_goliving" style="display: none;background: #0486FE;color: #fff;flex: 1 100%;">进入直播</p>
